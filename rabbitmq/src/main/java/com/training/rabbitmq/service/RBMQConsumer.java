7065044e5f9aeab8c69c97d2a52b21e42bf85200
Chibideska <archmage112@gmail.com>
2025-03-13 19:26:58 +0800

    Updated Consumer

--- rabbitmq/src/main/java/com/training/rabbitmq/service/RBMQConsumer.java
+++ rabbitmq/src/main/java/com/training/rabbitmq/service/RBMQConsumer.java
@@ -1,7 +1,7 @@
 package com.training.rabbitmq.service;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.oocl.training.rabbitmq.dto.MessageDTO;
+import com.training.rabbitmq.dto.MessageDTO;
 import lombok.RequiredArgsConstructor;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Service;
@@ -12,22 +12,20 @@ public class RBMQConsumer {
 
     @RabbitListener(queues = "${rabbit.queue.name}")
     public void processMessage(String payload) {
-        try{
-            ObjectMapper mapper = new ObjectMapper();
-            MessageDTO dto = mapper.readValue(payload, MessageDTO.class);
-            System.out.printf("Consuming Queue Data: Id %s, Name: %s, Remarks: %s %n", dto.getId(), dto.getMessage(), dto.getRemarks());
-        }catch (Exception e){
+        try {
+            System.out.println("Consuming Queue Data: " + payload);
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
 //    @RabbitListener(queues = "${rabbit.queue.name}")
 //    public void processMessage(String payload) {
-//        try{
+//        try {
 //            ObjectMapper mapper = new ObjectMapper();
-//            BLInfoDTO dto = mapper.readValue(payload, BLInfoDTO.class);
-//            System.out.printf("Consuming Queue Data: Id %s, Name: %s, Remarks: %s %n", dto.getId(), dto.getBlNumber(), dto.getRemarks());
-//        }catch (Exception e){
+//            MessageDTO dto = mapper.readValue(payload, MessageDTO.class);
+//            System.out.printf("Consuming Queue Data: Id %s, Name: %s, Remarks: %s %n", dto.getId(), dto.getMessage(), dto.getRemarks());
+//        } catch (Exception e) {
 //            e.printStackTrace();
 //        }
 //    }
